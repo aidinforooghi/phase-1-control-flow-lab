d9e45285190835ae3dd220f48b56b50163f4c1f0
Aidin <aidin.forooghi@gmail.com>
2023-03-19 01:25:33 -0400

    Passed 9 Tests

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance) {
   // Write your code here!
+  let greeting;
+  if (rideDistance <= 400) {
+    greeting = 'This one is on me!';
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    greeting = 'That will be twenty bucks.';
+  } else {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destinationCity) {
   // Write your code here!
+  return destinationCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount) {
   // Write your code here!
+  let response;
+  switch (tipAmount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
